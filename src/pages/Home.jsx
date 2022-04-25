facee551ea9412f3c6189e613929fb356dbda58c
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-25 01:47:15 -0500

    removed unused imports, removed form for product submit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,15 @@
 import {useState, useEffect} from "react";
 
 import Products from "../components/mainComponents/products/Products";
-import handleSubmitProduct from "../logic/productsLogic/handleSubmitProduct";
-import ModalWithResponseData from "../components/modals/ModalWithResponseData.jsx"
+import ModalWithResponseData from "../components/modals/ModalWithResponseData.jsx";
+import SubmitNewProductForm from "../components/forms/SubmitNewProduct";
 function Home() {
-	// const [isModalOpen, setIsModalOpen] = useState({
-	// 	isOpen: false,
-	// 	data: {},
-	// });
-	const [data, setData] = useState();
+	const [serverResponse, setServerResponse] = useState();
 	const [isInput, setIsInput] = useState({
 		name:"",
 		description:"",
 		imgUrl:"",
 	});
-	
-	useEffect(() => { // clears modal after 5 sec
-		if(data) {
-			const closeModal = setTimeout(() => {
-				setData();
-			}, 5000);
-			return () => clearTimeout(closeModal);
-		}
-	}, [data]);
 
 	function handleInputChange(event){ 
 		const value = event.target.value;
@@ -32,18 +19,19 @@ function Home() {
 		});
 	}
 
+	useEffect(() => { // clears modal after 5 sec
+		if(serverResponse) {
+			const closeModal = setTimeout(() => {
+				setServerResponse();
+			}, 5000);
+			return () => clearTimeout(closeModal);
+		}
+	}, [serverResponse]);
+
 	return (<>
 		<h2>Home page</h2>
-
-		<form onSubmit={async(event) => {
-			setData(await handleSubmitProduct(event, isInput));
-		}}>
-			<input placeholder="name of product" value={isInput.name} name="name" onChange={handleInputChange}/>
-			<input placeholder="description" value={isInput.description} name="description" onChange={handleInputChange}/>
-			<input placeholder="image url" value={isInput.imgUrl} name="imgUrl" onChange={handleInputChange}/>
-			<button type="submit">Submit</button>
-		</form>
-		{data && <ModalWithResponseData data={data}/>}
+		<SubmitNewProductForm setServerResponse={setServerResponse} isInput={isInput} handleInputChange={handleInputChange} />
+		{serverResponse && <ModalWithResponseData data={serverResponse}/>}
 		<main>
 			<Products />
 		</main>
