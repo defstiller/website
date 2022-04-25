543e09299de8e1c446e6057fce223108befafa13
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-25 01:16:16 -0500

    modal with inserted message from server

---
+++ src/components/modals/ModalWithResponseData.jsx
@@ -0,0 +1,12 @@
+import {useState, useEffect} from "react"
+
+function ModalWithResponseData({data}) {
+
+	return(
+		<div>
+			<p>{data}</p>
+		</div>
+	);
+}
+
+export default ModalWithResponseData;
