70252d805a406db2f53bb84fa0e0498986002ea0
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-25 23:46:27 -0500

    added fetch after succesfull submit

--- src/logic/productsLogic/handleSubmitProduct.js
+++ src/logic/productsLogic/handleSubmitProduct.js
@@ -1,12 +1,14 @@
 import {db} from "../firebase/firebaseCloud";
 import { collection, addDoc } from "firebase/firestore"; 
 
-const handleSubmitProduct = async(event, isInput) => {
+import getProducts from "../../components/mainComponents/products/getProducts";
+const handleSubmitProduct = async(event, isInput, setIsProducts) => {
 	event.preventDefault();
 	let response = "";
 	try {
 		const docRef = await addDoc(collection(db, "products"), isInput);
 		response = "Product added with ID: " + docRef.id;
+		getProducts(setIsProducts);
 	}
 	catch (error) {
 		response = "Error adding product: " + error;
