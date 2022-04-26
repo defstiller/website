b0adf4a2b979e7f0b6eb91eee83f495ad839ab52
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-25 23:43:40 -0500

    visually updated component, changed name

--- src/components/forms/SubmitNewProduct.jsx
+++ src/components/forms/SubmitNewProduct.jsx
@@ -1,15 +1,48 @@
 import handleSubmitProduct from "../../logic/productsLogic/handleSubmitProduct";
-function SubmitNewProductForm({setServerResponse, isInput, handleInputChange}) {
+import classes from "./submitNewProduct.module.css";
+function SubmitNewProduct({setServerResponse, isInput, handleInputChange, setIsProducts}) {
 	return (
-		<form onSubmit={async(event) => {
-			setServerResponse(await handleSubmitProduct(event, isInput));
-		}}>
-			<input placeholder="name of product" value={isInput.name} name="name" onChange={handleInputChange}/>
-			<input placeholder="description" value={isInput.description} name="description" onChange={handleInputChange}/>
-			<input placeholder="image url" value={isInput.imgUrl} name="imgUrl" onChange={handleInputChange}/>
-			<button type="submit">Submit</button>
-		</form>
+		<div className={classes.div}>
+			<form 
+				className={classes.form} 
+				onSubmit={async(event) => {
+					setServerResponse(await handleSubmitProduct(
+						event, 
+						isInput, 
+						setIsProducts
+					));
+				}}>
+				<input 
+					className={classes.input} 
+					placeholder="name of product" 
+					value={isInput.name} name="name" 
+					onChange={handleInputChange}
+				/>
+				<input 
+					className={classes.input} 
+					placeholder="description" 
+					value={isInput.description} 
+					name="description" 
+					onChange={handleInputChange}
+				/>
+				<input 
+					className={classes.input} 
+					placeholder="image url" 
+					value={isInput.imgUrl} 
+					name="imgUrl" 
+					onChange={handleInputChange}/>
+				<input 
+					className={classes.input} 
+					placeholder="price" 
+					value={isInput.price} 
+					name="price" 
+					onChange={handleInputChange} 
+					type="number"/>
+		
+				<button type="submit">Submit</button>
+			</form>
+		</div>
 	);
 }
 
-export default SubmitNewProductForm;
+export default SubmitNewProduct;
